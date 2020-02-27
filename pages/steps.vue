8e64ec8c85702818efd2c3d90df91f0e9f7f10f6
Félix Bouveret <bouveret.felix@gmail.com>
2020-02-27 16:47:26 +0100

    Fix progressbar

--- pages/steps.vue
+++ pages/steps.vue
@@ -1,34 +1,17 @@
 <template>
   <div>
-    <progress-bar :max="maxStepNumber" :current="currentStepNumber" />
     <steps />
   </div>
 </template>
 
 <script>
-import stepsScript from '~/services/steps'
-import progressBar from '../components/progressBar'
 import Steps from '~/components/steps'
 
 export default {
   name: 'stepsPage',
   components: {
-    Steps,
-    progressBar
+    Steps
   },
-  data() {
-    return {
-      stepsManager: stepsScript()
-    }
-  },
-  computed: {
-    currentStepNumber() {
-      return this.stepsManager.getStep().currentStep
-    },
-    maxStepNumber() {
-      return this.stepsManager.getStep().maxStep
-    }
-  }
 }
 </script>
 
