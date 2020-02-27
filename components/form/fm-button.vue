c669c11e86284345de28a4c9f5e13007eb55ed1a
Félix Bouveret <bouveret.felix@gmail.com>
2020-02-27 14:20:51 +0100

    Add button styles

--- components/form/fm-button.vue
+++ components/form/fm-button.vue
@@ -21,6 +21,14 @@ export default {
 
 <style lang="scss" scoped>
   .fm-button {
+    display: block;
+    padding: 16px 24px;
+    margin: auto;
+    border-radius: 8px;
+    border: 0;
+    font-size: 24px;
     background-color: $main-color;
+    color: white;
+    cursor: pointer;
   }
 </style>
