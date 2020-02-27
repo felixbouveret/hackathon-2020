cf74cd6f87221975b5053534a876f668e2fea8bb
Félix Bouveret <bouveret.felix@gmail.com>
2020-02-27 14:20:41 +0100

    Add homepage

---
+++ pages/steps.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <progress-bar :max="maxStepNumber" :current="currentStepNumber" />
+    <steps />
+  </div>
+</template>
+
+<script>
+import stepsScript from '~/services/steps'
+import progressBar from '../components/progressBar'
+import Steps from '~/components/steps'
+
+export default {
+  name: 'stepsPage',
+  components: {
+    Steps,
+    progressBar
+  },
+  data() {
+    return {
+      stepsManager: stepsScript()
+    }
+  },
+  computed: {
+    currentStepNumber() {
+      return this.stepsManager.getStep().currentStep
+    },
+    maxStepNumber() {
+      return this.stepsManager.getStep().maxStep
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
