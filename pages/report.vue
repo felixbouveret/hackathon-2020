a969558f942987d044d5962b113b1f712de99b44
Antoine Chassagne <antoine.chassagne@me.com>
2020-02-28 07:36:43 +0100

    :sparkles: Add loader

--- pages/report.vue
+++ pages/report.vue
@@ -8,7 +8,76 @@
     <solutions />
     <pied-de-page />
   </div>
-  <div v-else>Chargement...</div>
+  <div v-else class="loader">
+    <svg
+      version="1.1"
+      id="Layer_1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      x="0px"
+      y="0px"
+      width="48px"
+      height="60px"
+      viewBox="0 0 24 30"
+      style="enable-background:new 0 0 50 50;"
+      xml:space="preserve"
+    >
+      <rect
+        x="0"
+        y="0"
+        width="4"
+        height="10"
+        fill="#333"
+        transform="translate(0 12.8998)"
+      >
+        <animateTransform
+          attributeType="xml"
+          attributeName="transform"
+          type="translate"
+          values="0 0; 0 20; 0 0"
+          begin="0"
+          dur="0.6s"
+          repeatCount="indefinite"
+        ></animateTransform>
+      </rect>
+      <rect
+        x="10"
+        y="0"
+        width="4"
+        height="10"
+        fill="#333"
+        transform="translate(0 0.433533)"
+      >
+        <animateTransform
+          attributeType="xml"
+          attributeName="transform"
+          type="translate"
+          values="0 0; 0 20; 0 0"
+          begin="0.2s"
+          dur="0.6s"
+          repeatCount="indefinite"
+        ></animateTransform>
+      </rect>
+      <rect
+        x="20"
+        y="0"
+        width="4"
+        height="10"
+        fill="#333"
+        transform="translate(0 13.7669)"
+      >
+        <animateTransform
+          attributeType="xml"
+          attributeName="transform"
+          type="translate"
+          values="0 0; 0 20; 0 0"
+          begin="0.4s"
+          dur="0.6s"
+          repeatCount="indefinite"
+        ></animateTransform>
+      </rect>
+    </svg>
+  </div>
 </template>
 
 <script>
@@ -40,7 +109,9 @@ export default {
   },
   created() {
     this.generateReport().then(() => {
-      this.report = this.$store.state.report
+      setTimeout(() => {
+        this.report = this.$store.state.report
+      }, 1500)
     })
   }
 }
@@ -50,5 +121,18 @@ export default {
 .report-container {
   background-color: $skin;
   padding: 50px 0 100px 0;
+}
+
+.loader {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  svg path,
+  svg rect {
+    fill: $orange;
+  }
 }
 </style>
